e56c53c347118dc2f467d117fb467777c7f7823f
mclongyi <1069543556@qq.com>
2019-06-10 22:58:29 +0800

    提交XX

--- springdatajap/src/main/java/com/ly/springdatajap/SpringdatajapApplication.java
+++ springdatajap/src/main/java/com/ly/springdatajap/SpringdatajapApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringdatajapApplication {
 
     public static void main(String[] args) {
+		
         SpringApplication.run(SpringdatajapApplication.class, args);
+		
     }
 
 }
